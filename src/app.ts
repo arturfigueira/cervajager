371e6af354b34fa99325494ef09f06cc437b7f32
arturfigueira <arturfigueira@gmail.com>
2021-02-23 21:20:14 -0300

    Add Linter and TSConfig

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,5 @@
-export default class App{
-    run(){
-        console.log("Starting the application");
-    }
+export default class App {
+  run(): void {
+    console.log("Starting the application");
+  }
 }
